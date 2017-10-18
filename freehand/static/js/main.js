482a34be55a75a4053f977c281e2a35be4768c38
kanyin_me <kanyinsola_olugbake@yahoo.co.uk>
2017-10-18 08:09:34 +0100

    did order modal for cars as well, might use ajax

--- freehand/static/js/main.js
+++ freehand/static/js/main.js
@@ -35,10 +35,9 @@
 	    }
   	});
 
-  	/** OPEN DRIVER INFO MODAL
+  	/** OPEN DRIVER ORDER MODAL
   	*/
-    
-    $('.driver-license .view-btn').each(function(){
+    $('.license .view-btn').each(function(){
   		$(this).on('click', function(e){
         e.preventDefault();
   		  $(this).magnificPopup({
@@ -50,7 +49,7 @@
 
     /* CHANGE CHOOSE BUTTON TO CHECK CHECKBOX
     */
-  	$('.driver-order-form .choose-btn').each(function(){
+  	$('.order-form .choose-btn').each(function(){
       var btn = $(this),
         $checkbox = $('.driver-order-form #within');
       btn.on('click', function(e){
